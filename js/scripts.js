2057e70fdaad7988834fa1b4ed08b2a44dba6bed
Rodrigue Nzomo Nzogang <nzomo.rodrigue@tmrcomputing.com>
2024-10-04 11:43:54 +0100

    odoo 17

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function showDemo() {
+  alert("Demandez votre démonstration personnalisée !");
+}
+
+function showContactForm() {
+  alert("Contactez-nous dès aujourd'hui pour une consultation gratuite !");
+}
